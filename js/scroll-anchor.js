70945fc54c9518cdc935e992f5e5908c675748a0
Clifford Wong <clifford.wong@j2.com>
2018-09-14 10:14:24 -0700

    1st commit
    LESS samples for specific styles to be reused again elsewhere

---
+++ js/scroll-anchor.js
@@ -0,0 +1,14 @@
+//scroll to anchor
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 800, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
